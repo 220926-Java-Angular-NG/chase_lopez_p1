5e6665656be42ad041aee8ac8f5d659513023e49
Chase Lopez <chase.opez16@gmail.com>
2022-10-04 08:50:38 -0500

    Setup project and wrote basic functionality test

---
+++ src/test/java/MoneyTest.java
@@ -0,0 +1,33 @@
+import org.junit.jupiter.api.Test;
+
+public class MoneyTest {
+    @Test
+    public void registerNewAccount(){
+        //assertEquals(1,0);
+    }
+    @Test
+    public void userCanLogIn(){
+        //assertEquals(1,0);
+    }
+    @Test
+    public void noDuplicateUserNames(){
+        //will notify user if username is already taken
+        //assertEquals(1,0);
+    }
+    @Test
+    public void employeeCanSubmitReimbursementTicket(){
+        //assertEquals(1,0);
+    }
+    @Test
+    public void reimbursementTicketHasDescriptionAndAmount(){
+        //assertEquals(1,0);
+    }
+    @Test
+    public void pendingReimbursementTicketsStoredAndSeenByManagers(){
+        //assertEquals(1,0);
+    }
+    @Test
+    public void employeeCanSeePastSubmittedReimbursementTickets(){
+        //assertEquals(1,0);
+    }
+}
